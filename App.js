dd883f0093dbf1a37f8e5649c961d9bb246b28ac
Eric Niemeyer <eric.niemeyer@venturetec.net>
2022-09-12 14:28:23 -0400

    test

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test Eric Open up App.js to start working on your app!</Text>
+      <Text>Test Eric Open up App.js to start working on your app!</Text>
+      <Text>Test Eric Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
